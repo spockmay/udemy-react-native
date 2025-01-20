4ed6cf6e5e1ba60a8d0d9b598df2cabb027f19a4
Ryan May <spockmay@users.noreply.github.com>
2025-01-20 12:58:56 -0500

    setup background

--- project2/App.js
+++ project2/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function App() {
   return (
-    <View>
-      <StartGameScreen />
-    </View>
+    <LinearGradient style={styles.rootScreen} colors={['#72063c','#ddb52f']}>
+      <ImageBackground source={require('./assets/images/background.png')} resizeMode='cover' style={styles.rootScreen} imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  background: {
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  }
+});
