08ade0e8cc33aa16e0710fab0fb6dce86830ec00
Ryan May <spockmay@users.noreply.github.com>
2025-01-20 23:27:00 -0500

    Implement game logic

--- project2/screens/GameScreen.js
+++ project2/screens/GameScreen.js
@@ -1,17 +1,42 @@
+import { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton'
 
 import TitleText from '../components/TitleText';
+import Colors from '../utils/colors'
 
 
-function GameScreen(props) {   
+function GameScreen(props) {
+
+    const [guess, setGuess] = useState(50);
+    const [delta, setDelta] = useState(50);
+
+    function computeNewDelta(oldDelta) {
+        let d = parseInt(delta/2)
+        return d > 0 ? d : 1
+    }
+
+    function guessTooLow() {
+        setDelta(computeNewDelta(delta));
+        setGuess(guess + computeNewDelta(delta));
+    }
+
+    function guessTooHigh() {
+        setDelta(computeNewDelta(delta));
+        setGuess(guess - computeNewDelta(delta));
+    }
+
+    if (guess === props.numToGuess) {
+        console.log('end game')
+    }
+
     return (
         <View style={styles.screen}>
             <TitleText>Opponent's Guess</TitleText>
-            <Text>{props.numToGuess}</Text>
+            <Text style={styles.guessText}>{guess}</Text>
             <View style={styles.buttonRow}>
-                <PrimaryButton>+</PrimaryButton>
-                <PrimaryButton>-</PrimaryButton>
+                <PrimaryButton onPress={guessTooHigh}>+</PrimaryButton>
+                <PrimaryButton onPress={guessTooLow}>-</PrimaryButton>
             </View>
             <View>
                 <Text>Round Logs</Text>
@@ -28,5 +53,12 @@ const styles = StyleSheet.create({
     },
     buttonRow: {
         flexDirection: 'row',
+        justifyContent: 'center',
     },
+    guessText: {
+        textAlign: 'center',
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: Colors.secondary500,
+    }
 });
