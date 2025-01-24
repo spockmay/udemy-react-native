3c2b89d9c09924fbf66a5d6b3529d62aa23879c6
Ryan May <spockmay@users.noreply.github.com>
2025-01-24 18:42:13 -0500

    playing with drawer options

--- project4/App.js
+++ project4/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native'
-
+import AntDesign from '@expo/vector-icons/AntDesign';
 import UserScreen from './screens/UserScreen'
 import WelcomeScreen from './screens/WelcomeScreen'
 
@@ -11,9 +11,17 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
   <NavigationContainer>
-    <Drawer.Navigator>
-      <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-      <Drawer.Screen name="User" component={UserScreen} />
+    <Drawer.Navigator screenOptions={{
+              drawerActiveBackgroundColor: '#dcbff8',
+              }}>
+      <Drawer.Screen name="Welcome" component={WelcomeScreen} options={{
+        headerStyle: {backgroundColor: '#af7fdd'},
+        drawerLabel: 'Welcome Screen',
+        drawerIcon: ({color, size}) => <AntDesign name="home" size={size} color={color} />
+      }} />
+      <Drawer.Screen name="User" component={UserScreen} options={{
+        drawerIcon: ({color, size}) => <AntDesign name="user" size={size} color={color} />
+      }} />
     </Drawer.Navigator>
   </NavigationContainer>
   );
